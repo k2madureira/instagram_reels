78a0202a23f09d9124d67b139a84e3078dd4c619
k2madureira <lenilsonmadureira@gmail.com>
2020-08-19 10:22:11 -0300

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAx9IVm1AAw_I2UiJNHDCENJQhjnw8JF2U",
+  authDomain: "instagram-reels-631ee.firebaseapp.com",
+  databaseURL: "https://instagram-reels-631ee.firebaseio.com",
+  projectId: "instagram-reels-631ee",
+  storageBucket: "instagram-reels-631ee.appspot.com",
+  messagingSenderId: "638299956748",
+  appId: "1:638299956748:web:d19929ba0076c9a05bf029"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const bd = firebaseApp.firestore();
+
+export default bd;
