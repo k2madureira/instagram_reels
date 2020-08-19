71a9288147341e6ba14e155a5405b75f1f750a0e
k2madureira <lenilsonmadureira@gmail.com>
2020-08-19 10:49:41 -0300

    firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAx9IVm1AAw_I2UiJNHDCENJQhjnw8JF2U",
