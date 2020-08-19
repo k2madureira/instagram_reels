0d7957ff751a6d12e9a6016ee1b32dc2cb132b05
k2madureira <lenilsonmadureira@gmail.com>
2020-08-18 22:35:35 -0300

    footer css

--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -4,7 +4,11 @@ import './VideoFooter.css'
 
 import { Button, Avatar } from '@material-ui/core';
 
-import MusicNoteIcon from '@material-ui/icons/MusicNote'
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ModeCommentIcon from '@material-ui/icons/ModeComment';
+import SendIcon from '@material-ui/icons/Send';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 function VideoFooter({ channel, song, likes, shares, avatarSrc }) {
   return(
@@ -27,6 +31,24 @@ function VideoFooter({ channel, song, likes, shares, avatarSrc }) {
             </>
           )}
         </Ticker>
+      </div>
+      <div className="videoFooter_actions">
+        <div className="videoFooter_actionsLeft">
+            <FavoriteIcon fontSize="large"/>
+            <ModeCommentIcon fontSize="large"/>
+            <SendIcon fontSize="large"/>
+            <MoreHorizIcon fontSize="large"/>
+        </div>
+        <div className="videoFooter_actionsRight">
+            <div className="videoFooter_stat">
+              <FavoriteIcon />
+              <p>{likes}</p>
+            </div>
+            <div className="videoFooter_stat">
+              <ModeCommentIcon />
+              <p>{shares}</p>
+            </div>
+        </div>
       </div>   
     </div>
   )
